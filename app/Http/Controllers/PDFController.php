e02ab3ffd3c5d3893ea5e8d239f7c736712a6a25
J Alfonso Martínez <mtz.alfonso96@gmail.com>
2021-02-13 10:24:03 -0600

    Reparando control de login

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -8,6 +8,17 @@ use PDF;
 
 class PDFController extends Controller
 {
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+        $this->middleware('esAdmin');
+    }
+
     public function PDF(Request $request)
     {
         set_time_limit(0);
@@ -21,6 +32,6 @@ class PDFController extends Controller
 
         $pdf = PDF::loadView('pdf', compact('pacientes'))->setPaper('a4', 'landscape');
 
-        return $pdf->download('reporte_'.date('dmY_His').'.pdf');
+        return $pdf->download('reporte_' . date('dmY_His') . '.pdf');
     }
 }
